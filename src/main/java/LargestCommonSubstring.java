0ba1f06ef6ee1cca1c09787c1e1711b58ad0eecb
Tomasz S <tomasz.z.seidler@gmail.com>
2017-10-08 16:30:25 +0200

    Largest common substring solved

--- src/main/java/LargestCommonSubstring.java
+++ src/main/java/LargestCommonSubstring.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class LargestCommonSubstring {
     private static int[][] fillTable(String s1, String s2) {
@@ -18,24 +20,46 @@ public class LargestCommonSubstring {
     private static String[] readSequences(String s1, String s2, int[][]table) {
         int max = 0;
         int count = 0;
+        List<int[]> startIndices = new ArrayList<>();
         for (int i = 1; i < s1.length() + 1; i++) {
             for (int j = 1; j < s2.length() + 1; j++) {
-                if (table[i][j] == max) count++;
+                if (table[i][j] == max) {
+                    count++;
+                    int[] ind = {i, j};
+                    startIndices.add(ind);
+                }
                 if (table[i][j] > max) {
                     max = table[i][j];
+                    startIndices = new ArrayList<>();
+                    int[] ind = {i, j};
+                    startIndices.add(ind);
                     count = 1;
                 }
             }
         }
-        System.out.println(max + " " + count);
-        return null;
+        String[] result = new String[count];
+        for (int i = 0; i < count; i++) {
+            StringBuilder res = new StringBuilder("");
+            int index = startIndices.get(i)[0] - 1;
+            //int index = startIndices.get(i)[1] - 1;
+            for (int j = 0; j < max; j++) {
+                res.append(s1.substring(index, index + 1));
+                //res.append(s2.substring(index, index + 1));
+                index--;
+            }
+            result[i] = res.toString();
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        String s1 = "abab";
-        String s2 = "baba";
+        String s1 = "abasfdsbbbb";
+        String s2 = "babsssababbbba";
         int[][] table = fillTable(s1,s2);
-        System.out.println(Arrays.deepToString(table));
-        readSequences(s1, s2, table);
+        //System.out.println(Arrays.deepToString(table));
+        String[] LSC = readSequences(s1, s2, table);
+        for (String s : LSC) {
+            System.out.println(s);
+        }
     }
 }
